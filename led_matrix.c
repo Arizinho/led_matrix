53b32a8a93a5f8fefff897ede00f835fbd6a1643
Ariagildo L <ariagildo01@gmail.com>
2025-01-27 00:14:52 -0300

    Adiciona funções para controle de teclado matricial

--- led_matrix.c
+++ led_matrix.c
@@ -16,6 +16,60 @@
 //pino de saída
 #define OUT_PIN 7
 
+//número de linhas e colunas teclado
+#define ROWS 4
+#define COLUMNS 4
+
+//pinos de linhas e colunas do teclado matricial
+const uint8_t pin_rows[ROWS] = {2, 3, 4, 5};
+const uint8_t pin_columns[COLUMNS] = {16, 17, 18, 19};
+
+//matriz de mapeamento dos botões do teclado matricial
+const char key_map[ROWS][COLUMNS] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}
+};
+
+//função para inicializar os pinos do teclado matricial
+void teclado_init (void){
+    int i;
+    //inicializa os pinos de linha do teclado matricial como saída
+    for(i = 0; i < ROWS; i++){
+        gpio_init(pin_rows[i]);
+        gpio_set_dir(pin_rows[i], GPIO_OUT);
+    }
+
+    //inicializa os pinos de coluna do teclado matricial como entrada
+    for(i = 0; i < COLUMNS; i++){
+        gpio_init(pin_columns[i]);
+        gpio_set_dir(pin_columns[i], GPIO_IN);
+        gpio_pull_down(pin_columns[i]);
+    }
+
+}
+
+//função para ler tecla pressionada
+char teclado_get(void){
+    int r, c;
+    for (r = 0; r < ROWS; r++){
+        //coloca sinal de tensão alto nos pinos de linha do teclado
+        gpio_put(pin_rows[r], true);
+        //varre as colunas de 1 a 4 do teclado testando se algum botão na linha atual foi precionado
+        for (c = 0; c < COLUMNS; c++){
+            //se precionado retorna o caractere correspondente ao botão
+            if(gpio_get(pin_columns[c])){
+                gpio_put(pin_rows[r], false);
+                return key_map[r][c];
+            }
+        }
+        gpio_put(pin_rows[r], false);
+    }
+    //se nenhum botão for precionado retorna caractere nulo
+    return '\0';
+}
+
 //rotina para definição da intensidade de cores do led
 uint32_t matrix_rgb(double b, double r, double g)
 {
@@ -42,9 +96,13 @@ void desenho_pio(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r
     }
 }
 
+
+
 int main()
-{
+{   
+    char tecla;
     PIO pio = pio0; 
+    teclado_init();
     bool ok;
     uint16_t i;
     uint32_t valor_led;
@@ -65,7 +123,16 @@ int main()
     pio_matrix_program_init(pio, sm, offset, OUT_PIN);
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        //lê teclado
+        tecla = teclado_get();
+        switch (tecla)
+        {
+        case /* constant-expression */:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
     }
 }
