110ceff499636998ed7b13a6b22ba15123f64c1c
Ariagildo L <ariagildo01@gmail.com>
2025-01-28 15:21:15 -0300

    Refatora código e adiciona documentação na main

--- led_matrix.c
+++ led_matrix.c
@@ -16,6 +16,9 @@
 //pino de saída
 #define OUT_PIN 7
 
+//número de sequências que aparecerão na animação
+#define NUM_SEQUENCIAS 18
+
 //número de linhas e colunas teclado
 #define ROWS 4
 #define COLUMNS 4
@@ -168,7 +171,7 @@ bool cat_eyes[12][25] = {{0, 0, 0, 0, 0,
 
 //array de sequência dos quadros na animação
 //idenfica: {quadro a ser mostrado, quantidade de frames que o quadro será mostrado}
-uint8_t sequencia_quadros[18][2] = {{0,1},{1,1},{2,1},{3,1},{0,20},{0,1},{1,1},{2,1},{3,1},{0,20},{4,4},{5,8},{6,1},{7,1},{8,1},{9,1},{10,1},{11,1}};
+uint8_t sequencia_quadros[NUM_SEQUENCIAS][2] = {{0,1},{1,1},{2,1},{3,1},{0,20},{0,1},{1,1},{2,1},{3,1},{0,20},{4,4},{5,8},{6,1},{7,1},{8,1},{9,1},{10,1},{11,1}};
 
 //pinos de linhas e colunas do teclado matricial
 const uint8_t pin_rows[ROWS] = {2, 3, 4, 5};
@@ -267,21 +270,15 @@ void animacao_pio(bool *animacao_body, bool *animacao_eyes, PIO pio, uint sm){
 
 int main()
 {   
-    char tecla;
-    PIO pio = pio0; 
-    teclado_init();
-    bool ok;
-    uint16_t i;
-    uint32_t valor_led;
+    char tecla; //variavel para armazenar tecla pressionada
+    PIO pio = pio0; //seleciona a pio0
+    teclado_init(); //inicializa teclado
 
     //coloca a frequência de clock para 128 MHz, facilitando a divisão pelo clock
-    ok = set_sys_clock_khz(128000, false);
+    set_sys_clock_khz(128000, false);
 
     // Inicializa todos os códigos stdio padrão que estão ligados ao binário.
     stdio_init_all();
-
-    printf("iniciando a transmissão PIO");
-    if (ok) printf("clock set to %ld\n", clock_get_hz(clk_sys));
 
     //configurações da PIO
     uint offset = pio_add_program(pio, &pio_matrix_program);
@@ -294,40 +291,49 @@ int main()
         switch (tecla)
         {
         case 'A':
+            //Apaga matriz de LEDs
             desenha_cor(pio, sm, 0, 0, 0);
             break;
         
         case 'B':
+            //Acende cor azul em 100% na matriz de LEDs
             desenha_cor(pio, sm, 0, 0, 1);
             break;
 
         case 'C':
+            //Acende cor vermelha em 80% na matriz de LEDs
             desenha_cor(pio, sm, 0.8, 0, 0);
             break;
 
         case 'D':
+            //Acende cor verde em 50% na matriz de LEDs
             desenha_cor(pio, sm, 0, 0.5, 0);
             break;
 
         case '#':
+            //Acende cor branca em 20% na matriz de LEDs
             desenha_cor(pio, sm, 0.2, 0.2, 0.2);
             break;
 
         case '*':
+            //Entra em modo de armazenamento
             reset_usb_boot(0,0);
             break;
 
         case '1':
-            for (uint8_t j = 0; j < NUM_FRAMES; j++){
-                for (uint8_t i = 0; i < sequencia_imagens[j][1]; i++){
-                    animacao_pio(cat_body[sequencia_imagens[j][0]],cat_eyes[sequencia_imagens[j][0]],pio,sm);
+            //Mostra animação de gato na matriz de LEDs
+            for (uint8_t j = 0; j < NUM_SEQUENCIAS; j++){
+                for (uint8_t i = 0; i < sequencia_quadros[j][1]; i++){
+                    animacao_pio(cat_body[sequencia_quadros[j][0]],cat_eyes[sequencia_quadros[j][0]],pio,sm);
                     sleep_ms(125);
                 }
             }
+            //apaga LEDs ao final da animação
             desenha_cor(pio, sm, 0, 0, 0);
             break;
 
         default:
+            //Delay de 1ms para diminuir custo computacional da simulação no computador 
             sleep_ms(1);
             break;
         }
